e947e956d6b886609a15e911e26e265833555b90
MiroslavaMoh <miroslavamoh252@gmail.com>
2025-10-07 22:00:41 -0600

    feat(sidebar):add multi-level menu example + newicon link

--- resources/views/layouts/includes/admin/sidebar.blade.php
+++ resources/views/layouts/includes/admin/sidebar.blade.php
@@ -8,6 +8,9 @@
             'active' => request()->routeIs('admin.dashboard')
         ],
         [
+            'header' => 'Management'
+        ],
+        [
             'name' => 'Cats',
             'icon' => 'fa-solid fa-cat',
             'href' => route('admin.dashboard'),
@@ -39,14 +42,47 @@
             <ul class="space-y-1 font-medium">
                 @foreach ($links as $link)
                 <li>
-                    <a href= "{{ $link['href'] }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"> 
-                        <span class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white {{$link['active'] ? 'dark:group-hover:text-white': ""}}"> <!-- ? es un pequeño if para cambiar el estilo en activación --> 
-                            <i class="{{$link['icon']}}"></i> 
+
+                    @isset ($link ['header'])
+                    <div class="px-3 mt-4 mb-2 text-xs font-semibold text-gray-500 uppercase dark:text-gray-400"> 
+                        {{$link['header']}} 
+                    </div>
+                    @else
+                    <a href="{{ $link['href'] }}" 
+                    class="flex items-center p-2 text-gray-300 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <span class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 
+                                    group-hover:text-gray-300 dark:group-hover:text-white 
+                                    {{ $link['active'] ? 'dark:group-hover:text-white' : '' }}">
+                            <i class="{{ $link['icon'] }}"></i>
                         </span>
-                        <span class ="ms-3" > {{$link['name']}} </span>
+                        <span class="ms-3">{{ $link['name'] }}</span>
                     </a>
+                @endisset
                 </li>
                 @endforeach
+
+                 <li>
+            <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                  <svg class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
+                     <path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z"/>
+                  </svg>
+                  <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">E-commerce</span>
+                  <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                  </svg>
+            </button>
+            <ul id="dropdown-example" class="hidden py-2 space-y-2">
+                  <li>
+                     <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Products</a>
+                  </li>
+                  <li>
+                     <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Billing</a>
+                  </li>
+                  <li>
+                     <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Invoice</a>
+                  </li>
+            </ul>
+         </li>
             </ul>
         </div>
         </aside>
